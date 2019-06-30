0b64f417182bb4eea18c93357a22662e6a5dc339
tavisca-jkanathe <51927665+tavisca-jkanathe@users.noreply.github.com>
2019-06-30 23:55:33 +0530

    Update Game.cs
    implementation of a bowling ball game

--- bowling-ball/Game.cs
+++ bowling-ball/Game.cs
@@ -4,14 +4,37 @@ namespace BowlingBall
 {
     public class Game
     {
+        int[] rolls = new int[21];
+        int currentRoll = 0;
         public void Roll(int pins)
         {
-            throw new NotImplementedException();
+            rolls[currentRoll++] = pins;
         }
 
         public int GetScore()
         {
-            throw new NotImplementedException();
+            int score = 0;
+            int frameIndex = 0;
+			for (int frames = 0; frames < 10; frames++) {
+
+				//cases for strike and spare
+				if (rolls[frameIndex] == 10) 
+                {
+                    score = score + 10 + rolls[frameIndex + 1] + rolls[frameIndex + 2];
+                    frameIndex += 1;
+                }
+                else if(rolls[frameIndex] + rolls[frameIndex + 1] == 10)
+                {
+                    score = sccore + 10 + rolls[frameIndex + 2];
+                    frameIndex += 2;
+                }
+                else
+                {
+                    score = score + rolls[frameIndex] + rolls[frameIndex + 1];
+                    frameIndex += 2;
+                }
+            }
+			return score;
         }
 
     }
